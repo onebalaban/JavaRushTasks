c9b240521a414e9da3ff0f6a99ce577f294b4bd0
Roman Balaban <romanbalaban@outlook.com>
2017-08-30 16:40:43 +0300

    task 1919-2005 added

---
+++ 2.JavaCore/src/com/javarush/task/task19/task1919/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task19.task1919;
+
+/* 
+Считаем зарплаты
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
@@ -0,0 +1,54 @@
+package com.javarush.task.task20.task2003;
+
+import java.io.*;
+import java.lang.reflect.Array;
+import java.util.*;
+
+/* 
+Знакомство с properties
+*
+* рабочий вариант
+*
+*/
+public class Solution {
+    public static Map<String, String> properties = new HashMap<>();
+
+
+    public void fillInPropertiesMap() throws Exception {
+        //implement this method - реализуйте этот метод
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        reader.close();
+        InputStream inputStream = new FileInputStream(fileName);
+        load(inputStream);
+        OutputStream outputStream = new FileOutputStream(fileName);
+        save(outputStream);
+
+    }
+
+    public void save(OutputStream outputStream) throws Exception {
+        //implement this method - реализуйте этот метод
+        Properties prop = new Properties();
+        for (Map.Entry<String, String> pair: properties.entrySet()) {
+            prop.setProperty(pair.getKey(), pair.getValue());
+        }
+        prop.store(outputStream, null);
+        outputStream.close();
+    }
+
+    public void load(InputStream inputStream) throws Exception {
+        //implement this method - реализуйте этот метод
+        Properties prop = new Properties();
+        prop.load(inputStream);
+        properties.clear();
+        Set<String> keys = prop.stringPropertyNames();
+        for (String key:keys) {
+            properties.put(key, prop.getProperty(key));
+        }
+        inputStream.close();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
